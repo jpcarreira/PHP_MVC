bb0bd064329416dbfacdc45607056507b41acaeb
jpcarreira <jpmavcarreira@gmail.com>
2014-05-09 14:56:37 +0100

    added query

--- views/header.php
+++ views/header.php
@@ -11,6 +11,13 @@
         </title>
         
         <link rel="stylesheet" href="public/css/default.css">
+        
+        <script type="text/javascript" src="public/js/jquery.js">
+            
+        </script>
+        
+        <script type="text/javascript" src="public/js/custom.js"></script>
+        
     </head>
     
     <body>
