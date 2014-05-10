72e638676d17157c157ad588a2fca5c0d0b0d8d6
jpcarreira <jpmavcarreira@gmail.com>
2014-05-10 23:29:19 +0100

    fixed bug on not reloading page on delete action

--- views/dashboard/js/default.js
+++ views/dashboard/js/default.js
@@ -6,17 +6,22 @@ $(function()
         {
             $('#listInserts').append('<div>' + o[i].text + '<a class="del" rel="' + o[i].id + '" href="#">X</a></div>');
         }
-        
+       
         $('.del').click(function()
         {
             delItem = $(this);
             var id = $(this).attr('rel');
+            
             $.post('dashboard/xhrDeleteListing', {'id': id}, function(o)
             {
                 delItem.parent().remove();
-            });
+            }, 'json');
+            
+            location.reload();
+            
             return false; 
         });
+        
     }, 'json');
     
     
@@ -24,14 +29,13 @@ $(function()
     {
         var url = $(this).attr('action');
         var data = $(this).serialize();
-        //console.log(data);
         
         $.post(url, data, function(o)
         {
-            console.log(url);
-            //console.log(o);
             $('#listInserts').append('<div>' + o.text + '<a class="del" rel="' + o.id + '" href="#">X</a></div>');
         }, 'json');
+        
+        location.reload();
         
         return false; 
     });
