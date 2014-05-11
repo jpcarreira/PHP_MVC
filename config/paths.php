08e651ad6a74c99bd30c84abb5e48ce84c8e00be
jpcarreira <jpmavcarreira@gmail.com>
2014-05-11 16:51:00 +0100

    index.php now using built-in __autoload instead of multiple requires

--- config/paths.php
+++ config/paths.php
@@ -1,4 +1,9 @@
 <?php
 
-// current working directory
+// make sure you provide a TRAILING SLASH at the end of EVERY PATH!
+
+// path for the project
 define('URL', 'http://localhost/mvc/');
+
+// path for library folder
+define('LIBS', 'libs/');
--- index.php
+++ index.php
@@ -1,26 +1,14 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-// TOOD: use an auto-loader
-
-// requires for libs
-require 'libs/Bootstrap.php';
-require 'libs/Controller.php';
-require 'libs/Database.php';
-require 'libs/Model.php';
-require 'libs/Session.php';
-require 'libs/View.php';
-require 'libs/Hash.php';
-
 // config files
 require 'config/paths.php';
 require 'config/database.php';
 require 'config/constants.php';
+
+function __autoload($class)
+{
+    require LIBS . $class . ".php";
+}
 
 // instantiating the bootstrap
 $app = new Bootstrap();
