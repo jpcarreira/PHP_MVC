0ea5f26d164528d8058f255a985726d8224293c8
jpcarreira <jpmavcarreira@gmail.com>
2014-05-13 21:44:26 +0100

    added additional rendering in the view (as example)

--- controllers/error.php
+++ controllers/error.php
@@ -28,7 +28,9 @@ class Error extends Controller
         
         // calling the render function
         $this->view->render('header');
+        $this->view->render('error/inc/header');
         $this->view->render('error/index');
+        $this->view->render('error/inc/footer');
         $this->view->render('footer');
     }
 
---
+++ views/error/inc/footer.php
@@ -0,0 +1,3 @@
+<div id="errorHeader">
+	<h2>This is another rendering in views/error/inc/footer</h2>
+</div>
